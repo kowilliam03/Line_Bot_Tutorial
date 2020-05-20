fdde6d9524eced1edd19e4e22f544fe7011e51c6
kwn <kowilliam03@gmail.com>
2020-05-20 11:44:42 +0800

    modify app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 from configparser import ConfigParser
 
@@ -39,10 +39,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    def send_message(token, content):
+    def send_message(token, message):
         line_bot_api.reply_message(
             token,
-            TextMessage(text=content)
+            message
+            # TextMessage(text=content)
         )
 
     stocks = {
@@ -52,14 +53,19 @@ def handle_message(event):
     }
 
     text = event.message.text
-    message = ""
     if text in stocks:
         stock_price = get_stock_price(text)
-        message = f"{text} {stocks[text]} 目前股價爲: {stock_price}"
+        content = f"{text} {stocks[text]} 目前股價爲: {stock_price}"
     else:
-        message = "請輸入要查詢的股票代號：(1216 統一, 2330 臺積電, 3008 大立光)"
+        content = "請輸入要查詢的股票代號：(1216 統一, 2330 臺積電, 3008 大立光)"
 
-    send_message(event.reply_token, message)
+    # send_message(event.reply_token, TextMessage(text=content))
+    image_url = "https://i.imgur.com/TeoVsOc.png"
+    send_message(event.reply_token, ImageSendMessage(
+        original_content_url=image_url,
+        preview_image_url=image_url
+    ))
+
 
 if __name__ == "__main__":
     app.run()
